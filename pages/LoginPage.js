e4e3b69a67cc5e2727339a1317bafd203ca6419c
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2023-12-03 11:36:07 +0530

    changes in Login page

---
+++ pages/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div className='bg-green-600'>
+        <h1>Hello vikalp</h1>
+    </div>
+  )
+}
+
+export default LoginPage
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const index = () => {
   return (
     <div className=' flex justify-center items-center w-[100%] h-[100vh] ' >
-      <login/>
+      
     </div>
   )
 }
